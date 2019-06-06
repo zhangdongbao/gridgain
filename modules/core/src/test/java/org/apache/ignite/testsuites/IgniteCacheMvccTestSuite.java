e61051a50aaa86ffc0893259079722f4d28ea6b1
ipavlukhin <vololo100@gmail.com>
2019-06-06 17:05:46 +0300

    GG-19214: Prepare for repeated run

--- modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite.java
+++ modules/core/src/test/java/org/apache/ignite/testsuites/IgniteCacheMvccTestSuite.java
@@ -16,26 +16,7 @@
 
 package org.apache.ignite.testsuites;
 
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccClientReconnectTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccClusterRestartTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccConfigurationValidationTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccIteratorWithConcurrentTransactionTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccLocalEntriesWithConcurrentTransactionTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccOperationChecksTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccPartitionedCoordinatorFailoverTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccProcessorLazyStartTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccProcessorTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccRemoteTxOnNearNodeStartTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccReplicatedCoordinatorFailoverTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccScanQueryWithConcurrentTransactionTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccSizeWithConcurrentTransactionTest;
 import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccTransactionsTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccTxFailoverTest;
-import org.apache.ignite.internal.processors.cache.mvcc.CacheMvccVacuumTest;
-import org.apache.ignite.internal.processors.cache.mvcc.MvccCachePeekTest;
-import org.apache.ignite.internal.processors.cache.mvcc.MvccUnsupportedTxModesTest;
-import org.apache.ignite.internal.processors.datastreamer.DataStreamProcessorMvccPersistenceSelfTest;
-import org.apache.ignite.internal.processors.datastreamer.DataStreamProcessorMvccSelfTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -43,33 +24,33 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     CacheMvccTransactionsTest.class,
-    CacheMvccProcessorTest.class,
-    CacheMvccVacuumTest.class,
-    CacheMvccConfigurationValidationTest.class,
-
-    DataStreamProcessorMvccSelfTest.class,
-    DataStreamProcessorMvccPersistenceSelfTest.class,
-    CacheMvccOperationChecksTest.class,
-
-    CacheMvccRemoteTxOnNearNodeStartTest.class,
-
-    MvccUnsupportedTxModesTest.class,
-
-    MvccCachePeekTest.class,
-
-    // Concurrent ops tests.
-    CacheMvccIteratorWithConcurrentTransactionTest.class,
-    CacheMvccLocalEntriesWithConcurrentTransactionTest.class,
-    CacheMvccScanQueryWithConcurrentTransactionTest.class,
-    CacheMvccSizeWithConcurrentTransactionTest.class,
-
-    // Failover tests.
-    CacheMvccTxFailoverTest.class,
-    CacheMvccClusterRestartTest.class,
-    CacheMvccPartitionedCoordinatorFailoverTest.class,
-    CacheMvccReplicatedCoordinatorFailoverTest.class,
-    CacheMvccProcessorLazyStartTest.class,
-    CacheMvccClientReconnectTest.class
+//    CacheMvccProcessorTest.class,
+//    CacheMvccVacuumTest.class,
+//    CacheMvccConfigurationValidationTest.class,
+//
+//    DataStreamProcessorMvccSelfTest.class,
+//    DataStreamProcessorMvccPersistenceSelfTest.class,
+//    CacheMvccOperationChecksTest.class,
+//
+//    CacheMvccRemoteTxOnNearNodeStartTest.class,
+//
+//    MvccUnsupportedTxModesTest.class,
+//
+//    MvccCachePeekTest.class,
+//
+//    // Concurrent ops tests.
+//    CacheMvccIteratorWithConcurrentTransactionTest.class,
+//    CacheMvccLocalEntriesWithConcurrentTransactionTest.class,
+//    CacheMvccScanQueryWithConcurrentTransactionTest.class,
+//    CacheMvccSizeWithConcurrentTransactionTest.class,
+//
+//    // Failover tests.
+//    CacheMvccTxFailoverTest.class,
+//    CacheMvccClusterRestartTest.class,
+//    CacheMvccPartitionedCoordinatorFailoverTest.class,
+//    CacheMvccReplicatedCoordinatorFailoverTest.class,
+//    CacheMvccProcessorLazyStartTest.class,
+//    CacheMvccClientReconnectTest.class
 })
 public class IgniteCacheMvccTestSuite {
 }
